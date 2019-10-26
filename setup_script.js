cd719562678dda54274557bf62096615c83525aa
victoria-huang-yining <yining.huang@edu.uwaterloo.ca>
2019-10-26 16:57:29 -0400

    Add page 2 and js
    need more data validation!

--- setup_script.js
+++ setup_script.js
@@ -1,5 +1,6 @@
 // Script file for HTML page: setup.html
 var finalParseData;
+var numStudents;
 // Show/hide element based on div id and current state
 function toggleVisiblity(tab) {
   var element = document.getElementById(tab);
@@ -22,13 +23,39 @@ function generateTeams() {
   window.location.href = "teams.html";
 }
 
-// Get number of students from the input file (read as an array)
-function getNumStudents(array) {
-  numStudents = array.length;
+function upload() {
+  let input = document.getElementById('student_data');
+  if (input.files && input.files[0]) {
+      console.log("FOUND FILE")
+      var myFile = input.files[0];
+      var reader = new FileReader();
+      reader.addEventListener('load', function (e) {
+          let csvdata = e.target.result;
+          let parsedata = [];
+          let newLinebrk = csvdata.split("\n");
+          for(let i = 0; i < newLinebrk.length; i++) {
+              parsedata.push(newLinebrk[i].split(","))
+          }
+
+          finalParseData = parsedata;
+          console.log(finalParseData);
+
+      });
+      reader.readAsBinaryString(myFile);
+  }
+
+}
+
+function getNumStudents(array){
+  num = array.length - 1;
+  numStudents = num;
+  document.getElementById("insertNumStudents").innerHTML = numStudents;
+  console.log(numStudents);
 }
 
 // Get number of teams, calculated from number of students and team size
-function getNumTeams(numStudents, inputID){
-  teamSize = document.getElementById(inputID).value;
-  numTeams = (numStudents - 1)/teamSize
+function getNumTeams(num){
+  teamSize = document.getElementById("teamSize").value;
+  numTeams = num/teamSize;
+  document.getElementById("insertNumTeams").innerHTML = numTeams;
 }
