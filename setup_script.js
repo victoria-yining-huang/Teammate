6fec74b97b9c1ed0ae75d712274ccb490262514f
victoria-huang-yining <yining.huang@edu.uwaterloo.ca>
2019-10-26 14:35:05 -0400

    commit wip before pulling master

--- setup_script.js
+++ setup_script.js
@@ -21,3 +21,14 @@ function generateTeams() {
   event.preventDefault();
   window.location.href = "teams.html";
 }
+
+// Get number of students from the input file (read as an array)
+function getNumStudents(array) {
+  numStudents = array.length;
+}
+
+// Get number of teams, calculated from number of students and team size
+function getNumTeams(numStudents, inputID){
+  teamSize = document.getElementById(inputID).value;
+  numTeams = (numStudents - 1)/teamSize
+}
