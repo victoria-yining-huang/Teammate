e2f73157db1b0ca87d44fbe67a08fce4a925f614
ndhingra96 <ndhingra@edu.uwaterloo.ca>
2019-10-26 13:42:58 -0400

    Added Tab 1, added javascript for parsing uploaded data

--- setup_script.js
+++ setup_script.js
@@ -1,5 +1,5 @@
 // Script file for HTML page: setup.html
-
+var finalParseData;
 // Show/hide element based on div id and current state
 function toggleVisiblity(tab) {
   var element = document.getElementById(tab);
@@ -20,4 +20,24 @@ function nextTab(thisTab, nextTab) {
 function generateTeams() {
   event.preventDefault();
   window.location.href = "teams.html";
+}
+
+function upload() {
+  let input = document.getElementById('student_data');
+  if (input.files && input.files[0]) {
+      console.log("FOUND FILE")
+      var myFile = input.files[0];
+      var reader = new FileReader();
+      reader.addEventListener('load', function (e) {
+          let csvdata = e.target.result; 
+          let parsedata = [];
+          let newLinebrk = csvdata.split("\n");
+          for(let i = 0; i < newLinebrk.length; i++) {
+              parsedata.push(newLinebrk[i].split(","))
+          }
+
+          finalParseData = parsedata;
+      });
+      reader.readAsBinaryString(myFile);
+  }
 }
