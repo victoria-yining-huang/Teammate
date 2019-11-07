19e798f824128ee0e0bda0f750746fdb778d6646
ndhingra96 <ndhingra@edu.uwaterloo.ca>
2019-11-07 13:46:47 -0500

    Added instructions on each page and made some UI button changes.

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <!DOCTYPE html>
 <html>
+<style>
+.w3-button{
+ box-shadow: 0 20px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+ 
+}
+a:hover {
+  color: blue;
+}
+ #left{ 
+        padding: 10px;
+        float:left;  
+        width:60%; 
+        height:500px; 
+} 
+#right{ 
+      
+      float:right; 
+      width:40%; 
+      height:300px; 
+      border-left: 3px solid;
+      border-color: #FFD700 ;
+      padding-left: 20px;
+
+}
+</style>
   <!-- CSS Classes -->
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
   <link rel="stylesheet" type="text/css" href="www/main.css" />
@@ -15,7 +40,6 @@
     <div class="w3-top">
       <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
         <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">teammate</a>
-        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Instructions</a>
       </div>
     </div>
 
@@ -30,8 +54,10 @@
             <h3>Upload Student Data</h3>
           </div>
           <div class="w3-container tab-content">
+            <div>
+            <div id="left">
             <h3>Start the team generating process by:</h3>
-            <h5>Upload the exported student data from 'Team 7 output' as a CSV file.</h5>
+            <h4>Upload the exported student data from 'Team 7 output' as a CSV file.</h4>
            <!-- <input type="file" id="student_data" />
             <button onclick="uploadStudentData()" class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Upload Student Data</button>-->
             <input id="student_data" type="file" onchange="parseStudentData()"hidden/>
@@ -41,16 +67,34 @@
             <p id="student-error" style="color:red; font-weight: bold;"p>
             <p id="student-successful" style="color:green; font-weight: bold;" p>
             <br />
-            <h5>See a sample table for the student data file.</h5>
-            <a href="data\students.csv" target="_blank" class="w3-margin-top">
-              Download Sample File
+            <h4>See a sample table for the student data file.</h4>
+            <h4><a href="data\students.csv" target="_blank" class="w3-margin-top"><b>
+              Download Sample File</b></h4>
             </a>
             <!--	<br> <h5>2) Uploading an existing team file as a .xlsx file</h5> -->
             <!--	
             	<input type="file" id="student_data"/>
             	<button onclick="upload()"class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Upload</button>
+
             -->
+            </div> 
+            <!--- Adding instructions for page 1-->
+          <div id= "right">
+            <h3>Instructions</h3>
+            
+            <h5>1) Access the file given to you by Team-7.</h5>
+            <h5>2) Make sure it is in <b>.csv</b> format.</h5>
+            <h5>3) If you want to look at an example of a sample file, <br> download the sample file.</h5>
+            <h5>4) Click on the upload button and select the Team-7 file.</h5>
+            <h5>5) A Next Step button will appear, click on it.</h5>
           </div>
+
+            
+  
+          </div>
+          </div>
+
+         
           <div class="w3-container w3-blue tab-footer page-footer">
             <button id="nextstep" class="w3-button w3-black w3-xlarge w3-hover-white" onclick="getNumStudents(student_data);nextTab('tab-1', 'tab-2')">
               Next Step
@@ -58,16 +102,19 @@
           </div>
         </div>
         <div id="tab-2" class="hidden">
-          <div>
+          <div>    
             <div class="w3-container tab-header w3-center">
               <h3>Configure Teams</h3>
             </div>
             <div class="w3-container tab-content">
+              
+              <div id="left">
               <div>
                 <span id="insertNumStudents" class="w3-xlarge"></span>
                 <span class="w3-xlarge"> students found in the inputted file.</span>
               </div>
-              <p class="w3-xlarge"><u>Configure: </u></p>
+
+              <p class="w3-xlarge"><b>Configure:</b></p>
               <p class="w3-xlarge">Please enter the number of members per team</p>
               <p class="w3-xlarge">Team Size: <input type="number" min="1" id="teamSize" step="1" required="required" oninput="getNumTeams(numStudents)" /></p>
               
@@ -75,8 +122,18 @@
                 <span class="w3-xlarge">Number of Teams: </span>
                 <span class="w3-xlarge" id="insertNumTeams"></span>
               </div>
+
             </div>
+             <div id= "right">
+            <h3>Instructions</h3>
+            
+            <h5>1) Check if the number of students in the uploaded file is same as the one displayed on the top of the page.</h5>
+            <h5>2) Enter the desired team size. The value that you enter is the minimum number of students that would be placed within a team. If the number of students cannot be divided evenly within the entered team size, then some teams may have <b>team size +1</b> students. </h5>
+            <h5>3) The number of teams for the model will be calculated and displayed below the text field! </h5>
           </div>
+          </div>
+          </div>
+
           <div class="w3-container w3-blue tab-footer page-footer">
             <button class="w3-button w3-black w3-xlarge w3-hover-white" onclick="nextTab('tab-2', 'tab-1')">
               Previous Step
@@ -91,6 +148,7 @@
             <h3>Upload Student Conflicts</h3>
           </div>
           <div class="w3-container tab-content">
+          <div id ="left">
             <h5>Please upload the conflict file generated by Team 7 as a CSV file.</h5>
             <!--<form id="formid" name="form" method="post" action="upload.php" enctype="multipart/form-data" >-->
               <input id="conflict_data" type="file" name="my_file" hidden/>
@@ -101,10 +159,22 @@
             <p id="successful" style="color:green; font-weight: bold;" p>
             <br />
             <h5>See a sample table for the student conflict file.</h5>
-            <a href="data\conflicts.csv" target="_blank" class="w3-margin-top">
-              Download Sample File
+            <a href="data\conflicts.csv" target="_blank" class="w3-margin-top"><b><h4>
+              Download Sample File</h4></b>
             </a>
           </div>
+
+          <div id= "right">
+            <h3>Instructions</h3>
+            <h5>1) Click the upload conflict button. </h5>
+            <h5>2) Find the conflicts file provided to you by Team-7 and click <b>Open</b>.</h5>
+            <h5>2) Make sure it is in <b>.csv</b> format.</h5>
+            <h5>3) If you want to look at an example of a sample file, <br> download the sample file.</h5>
+            <h5>4) Click on the upload button and select the Team-7 file.</h5>
+            <h5>5) A Generate Teams button will appear, click on it.</h5>
+          </div>
+
+        </div>
           <div class="w3-container w3-blue tab-footer page-footer">
             <button class="w3-button w3-black w3-xlarge w3-hover-white" onclick="nextTab('tab-3', 'tab-2')">
               Previous Step
