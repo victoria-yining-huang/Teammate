8b32782603da7a3be808226aa093fcf86e3a8297
irkemp <ians.emails@gmail.com>
2019-11-13 14:26:14 -0500

    Fix

--- app.py
+++ app.py
@@ -51,6 +51,11 @@ def index():
     return "<h1>Welcome to our server !!</h1>"
 
 
+@app.route('/ping')
+def ping():
+    return("ping!")
+
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
