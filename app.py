5066dfc30a50326183c9300bc41b926078832819
irkemp <ians.emails@gmail.com>
2019-11-14 15:21:26 -0500

    create model framework

--- app.py
+++ app.py
@@ -2,50 +2,8 @@
 from flask import Flask, request, jsonify, send_from_directory
 from time import sleep
 import multiprocessing as mp
+from model_manager import start_model, get_model_status
 app = Flask(__name__)
-
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
 
 
 @app.route('/app/<path:path>')
@@ -53,45 +11,30 @@ def send_js(path):
     return send_from_directory('pages', path)
 
 
-def generateTeams():
-    print("start")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("continue")
-    sleep(5)
-    print("stop")
-
-
 @app.route('/start', methods=['POST'])
 def start():
-    key = request.form.get('key')
-    print(key)
+    content = request.get_json()
 
-    process = mp.Process(target=generateTeams)
-    process.start()
+    result = start_model(content)
 
-    return(jsonify({"Message": "Model started", "METHOD": "POST"}))
+    return(jsonify({result}))
 
 
-@app.route('/check', methods=['GET'])
+@app.route('/check', methods=['POST'])
 def check():
-    key = request.form.get('key')
-    print(key)
+    content = request.get_json()
 
-    print(mp.current_process())
+    #status = get_model_status(content)
 
-    return(jsonify({"ActiveThreads": mp.current_process().name, "METHOD": "POST"}))
+    return(jsonify({
+
+    }))
+
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    print("stop!")
 
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
---
+++ model_manager.py
@@ -0,0 +1,70 @@
+import multiprocessing as mp
+from time import sleep
+import random
+import string
+
+manager = mp.Manager()
+model_dict = manager.dict()
+init_model_dict()
+process = mp.Process()
+
+
+def generate_key(length):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(length))
+
+
+def init_model_dict():
+    model_dict["key"] = None
+    model_dict["status"] = "ready"
+    model_dict["output"] = None
+
+
+def generateTeams(model_input, model_dict):
+    print("start")
+    sleep(5)
+    model_dict["status"] = "done"
+    model_dict["output"] = {
+        "test": 1,
+        "vals": [1, 2, 3]
+    }
+    print("stop")
+
+
+def start_model(content):
+    global process
+    global model_dict
+
+    key = generate_key(30)
+
+    model_dict["key"] = key
+    model_dict["status"] = "running"
+
+    try:
+        if not process.is_alive():
+            process = mp.Process(target=generateTeams,
+                                 args=(content, model_dict))
+            process.start()
+    except Exception as e:
+        return({
+            "Status": "failed",
+            "Message": e,
+            "METHOD": "POST"
+        })
+
+    return({
+        "Status": "started",
+        "Key": key
+    })
+
+
+def get_model_status():
+    if model_dict["status"] == "running":
+        print("running")
+    elif model_dict["status"] == "done":
+        print("done")
+        print(model_dict["output"])
+        model_dict["status"] = "new"
+        model_dict["output"] = {}
+    elif model_dict["status"] == "new":
+        print("not created yet")
