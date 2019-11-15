655fc1cc9660cb9aa43940daff9cb98ad9883d92
irkemp <ians.emails@gmail.com>
2019-11-14 20:29:01 -0500

    update api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 from time import sleep
 import multiprocessing as mp
-import model_manager
+import model_manager as mm
 app = Flask(__name__)
 
 
@@ -14,7 +14,7 @@ def send_js(path):
 @app.route('/start-model', methods=['POST'])
 def start():
     content = request.get_json()
-    result = start_model(content)
+    result = mm.start_model(content)
     return(jsonify(result))
 
 
@@ -22,13 +22,13 @@ def start():
 def check():
     data = request.get_json()
     key = data["key"]
-    result = get_model_status(key)
+    result = mm.get_model_status(key)
     return(jsonify(result))
 
 
-@app.route('/ping', methods=['POST'])
+@app.route('/ping', methods=['GET'])
 def ping():
-    return(jsonify(ping_model()))
+    return(jsonify(mm.ping_model()))
 
 
 @app.route('/stop-model', methods=['POST'])
