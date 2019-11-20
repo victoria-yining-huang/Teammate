40d62ae0036a7d6f7303a54858af960fcbb688cd
irkemp <ians.emails@gmail.com>
2019-11-20 18:44:59 -0500

    Refactor server HTTP response structure

--- static/scripts/model_script.js
+++ static/scripts/model_script.js
@@ -24,17 +24,14 @@ async function timer(key) {
 		if (resp["Status"] == "failed") {
 			alert(resp["Message"]);
 		} else {
-			if (resp["Body"]["ModelIsFinished"]) {
+			if (resp["ModelIsFinished"]) {
 				setTimer = null;
 				window.clearInterval(setModelTimer);
 				document.getElementById("modelRuntime").innerHTML = "Model Finished";
-				output = processResult(resp["Body"]["Result"]);
+				output = processResult(resp["Result"]);
 				sessionStorage.setItem("output", JSON.stringify(output));
-				console.log("OUTPUT");
-				console.log(output);
 				window.location.href = "/app/teams.html";
 			} else {
-				console.log("!! model still running");
 				timer(key);
 			}
 		}
@@ -42,23 +39,27 @@ async function timer(key) {
 }
 
 function start() {
-	let model_input = JSON.stringify(prepareData());
-
-	$.ajax({
-		url: "/start-model",
-		type: "post",
-		data: model_input,
-		contentType: "application/json; charset=utf-8",
-		success: function (resp) {
-			console.log(resp);
-			const key = resp["Key"];
-			setModelTimer = setInterval(modelTimer, 1000);
-			modelRuntime = 0;
-			//sessionStorage.setItem("model_key", key);
-			console.log("start timed checks");
-			timer(key);
-		}
-	});
+	const prep_data = prepareData();
+	if (prep_data != null) {
+		let model_input = JSON.stringify(prep_data);
+		$.ajax({
+			url: "/start-model",
+			type: "post",
+			data: model_input,
+			contentType: "application/json; charset=utf-8",
+			success: function (resp) {
+				const key = resp["Key"];
+				setModelTimer = setInterval(modelTimer, 1000);
+				modelRuntime = 0;
+				timer(key);
+			},
+			error: function (resp) {
+				handleError(resp)
+			}
+		});
+	} else {
+		handleError("Input data is invalid.")
+	}
 }
 
 function check(key) {
@@ -72,11 +73,18 @@ function check(key) {
 				key: key
 			}),
 			success: function (resp) {
-				console.log(resp);
 				resolve(resp);
+			},
+			error: function (resp) {
+				handleError(resp)
 			}
 		});
 	});
+}
+
+function handleError(resp) {
+	console.log("error!")
+	console.log(resp)
 }
 
 function ping() {
@@ -94,54 +102,58 @@ function prepareData() {
 	const conflicts = JSON.parse(sessionStorage.getItem("conflicts"));
 	const team_size = sessionStorage.getItem("team_size");
 
-	let num_students = students.length;
-	let num_teams =
-		Math.floor(num_students / team_size) +
-		Math.min(1, num_students % team_size);
+	if (students != null && conflicts != null && team_size != null) {
 
-	// get ids vector
-	const ids = students.map(function (value, index) {
-		return value[0];
-	});
+		let num_students = students.length;
+		let num_teams =
+			Math.floor(num_students / team_size) +
+			Math.min(1, num_students % team_size);
 
-	// deduplicate conflicts
-	var conflicts_new = [];
-	for (var i = 0; i < conflicts.length; i++) {
-		const person_a = ids.indexOf(conflicts[i][0]);
-		const person_b = ids.indexOf(conflicts[i][3]);
-		const conflict = [person_a, person_b];
-		const conflict_inverse = [person_b, person_a]
-		if (!conflicts_new.includes(conflict_inverse)) {
-			conflicts_new.push(conflict)
+		// get ids vector
+		const ids = students.map(function (value, index) {
+			return value[0];
+		});
+
+		// deduplicate conflicts
+		var conflicts_new = [];
+		for (var i = 0; i < conflicts.length; i++) {
+			const person_a = ids.indexOf(conflicts[i][0]);
+			const person_b = ids.indexOf(conflicts[i][3]);
+			const conflict = [person_a, person_b];
+			const conflict_inverse = [person_b, person_a]
+			if (!conflicts_new.includes(conflict_inverse)) {
+				conflicts_new.push(conflict)
+			}
 		}
+
+		// get gpa vector
+		let gpas = students.map(function (value, index) {
+			return parseInt(value[4]);
+		});
+
+		// get gender vector
+		let genders = students.map(function (value, index) {
+			return value[5].toLowerCase();
+		});
+
+		return ({
+			num_students: num_students,
+			num_teams: num_teams,
+			team_size: parseInt(team_size),
+			conflicts: conflicts_new,
+			gpas: gpas,
+			genders: genders
+		});
 	}
-
-	// get gpa vector
-	let gpas = students.map(function (value, index) {
-		return parseInt(value[4]);
-	});
-
-	// get gender vector
-	let genders = students.map(function (value, index) {
-		return value[5].toLowerCase();
-	});
-
-	return ({
-		num_students: num_students,
-		num_teams: num_teams,
-		team_size: parseInt(team_size),
-		conflicts: conflicts_new,
-		gpas: gpas,
-		genders: genders
-	});
+	else {
+		return null;
+	}
 }
 
 function processResult(result) {
 
 	const students = JSON.parse(sessionStorage.getItem("students"));
 	const conflicts = JSON.parse(sessionStorage.getItem("conflicts"));
-
-	console.log(result)
 
 	var output = {
 		model: {
@@ -165,7 +177,6 @@ function processResult(result) {
 
 	// Create students
 	students.forEach(function (student) {
-		console.log(student)
 		const id = student[0];
 		output["people"][id] = {
 			id: id,
